bd4022819f89f32953de580acae1fbb3f7c6719d
Dima Sichkar <dmytrosichkar@gmail.com>
2022-05-20 12:13:48 +0300

    service enable and start

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,12 +15,18 @@ install Libxml2 libxml2
 install Libxslt libxslt
 install Rbenv rbenv
 install Apache apache24
+install Mysql mysql57-server
+
+sudo service apache24 enable
+sudo service apache24 start
+sudo service mysql-server enable
+sudo service mysql-server start
 
 rbenv install 2.7.4
 eval "$(rbenv init - bash)"
 echo 'eval "$(rbenv init - bash)"' > .bash_profile
 rbenv global 2.7.4
 
-gem install sqlite3 -- --with-sqlite3-dir=/usr/local --with-sqlite3-lib=/usr/local/lib
-gem install bundler
+gem install sqlite3 -N -- --with-sqlite3-dir=/usr/local --with-sqlite3-lib=/usr/local/lib
+gem install bundler passenger -N
 
