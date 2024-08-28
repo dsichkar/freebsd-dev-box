4f3cc02e4ef140f80331681b6182c3d13fcd7570
Dima Sichkar <dmytrosichkar@gmail.com>
2024-08-28 15:48:56 +0300

    Rails 7

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,29 +6,17 @@ function install {
   sudo pkg install -yf "$@" >/dev/null 2>&1
 }
 
-install Bash bash
 install Curl curl
 install Git git
-install Ruby-build ruby-build
-install Sqlite3 sqlite3
-install Libxml2 libxml2
-install Libxslt libxslt
-install Rbenv rbenv
 install Apache apache24
 install Mysql mysql57-server
+install Rails7 rubygem-rails70
 
 sudo service apache24 enable
 sudo service apache24 start
 sudo service mysql-server enable
 sudo service mysql-server start
 
-rbenv install 2.7.4
-eval "$(rbenv init - bash)"
-echo 'eval "$(rbenv init - bash)"' > .bash_profile
-rbenv global 2.7.4
+sudo gem install bundler passenger -N
 
-gem install sqlite3 -N -- --with-sqlite3-dir=/usr/local --with-sqlite3-lib=/usr/local/lib
-gem install bundler passenger -N
-
-yes | passenger-install-apache2-module
-
+yes | sudo passenger-install-apache2-module
