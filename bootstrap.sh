7cae921bb26bec754e9eb39480ced89587877fee
Dima Sichkar <dmytrosichkar@gmail.com>
2024-08-29 11:50:08 +0300

    Passenger

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,9 @@ install Curl curl
 install Git git
 install Nginx nginx-full
 install Rails7 rubygem-rails70
+install Passenger rubygem-passenger-nginx
 
 sudo service nginx enable
 sudo service nginx start
 
-sudo gem install bundler passenger -N
+sudo gem install bundler -N
