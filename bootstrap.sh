1cd62071befc6903ce28676142118fbcf3b4f112
Dima Sichkar <dmytrosichkar@gmail.com>
2024-08-29 15:07:02 +0300

    Redis, rails71

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,13 @@ function install {
 install Curl curl
 install Git git
 install Nginx nginx-full
-install Rails7 rubygem-rails70
+install Rails7 rubygem-rails71
+install Redis redis
 install Passenger rubygem-passenger-nginx
 
 sudo service nginx enable
 sudo service nginx start
+sudo service redis enable
+sudo service redis start
 
 sudo gem install bundler -N
